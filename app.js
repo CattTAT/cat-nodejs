97bf76dcbec058f40a7480acfbb979347652959b
teresa.fung <teresa.fung@itchannel.com.hk>
2024-02-26 16:14:30 +0800

    feat: return random image from native image list

--- app.js
+++ app.js
@@ -2,16 +2,40 @@
 
 // Load HTTP module
 const http = require("http");
+const fs = require("fs");
+
 const hostname = "127.0.0.1";
 const port = 8000;
 
+const imgList = [
+  "Boss_MEOW.jpeg",
+  "Debug_MEOW.jpeg",
+  "Double_Meow.jpeg",
+  "Sleepy_MEOW.jpeg",
+];
+
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(
-    JSON.stringify({
-      data: "Meow Meow study, Meow Meow learn, Meow Meow practice, Meow Meow work hard, Meow Meow grow, Meow Meow success!",
-    })
-  );
+  switch (req.url) {
+    case "/":
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(
+        JSON.stringify({
+          data: "Meow Meow study, Meow Meow learn, Meow Meow practice, Meow Meow work hard, Meow Meow grow, Meow Meow success! test",
+        })
+      );
+      break;
+    case "/img":
+      fs.readFile(
+        "./image/" + imgList[Math.floor(Math.random() * 4)],
+        function (err, data) {
+          if (err) throw err;
+          // res.write(data);
+          res.writeHead(200, { "Content-Type": "image/jpeg" });
+          res.end(data, "base64");
+        }
+      );
+      break;
+  }
 });
 
 // Prints a log once the server starts listening
