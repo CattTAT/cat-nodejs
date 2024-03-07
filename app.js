8d78fbc8952a7fbdc2bb98678d34805e3dd0f0a4
CattTAT <chinchin2611@gmail.com>
2024-03-08 02:49:35 +0800

    feat: return db query and display with /meow path

--- app.js
+++ app.js
@@ -15,36 +15,37 @@ const imgList = [
 ];
 
 const sqlite3 = require("sqlite3").verbose();
-
-const query = () => {
-  const result = {};
-  // open database in memory
-  let db = new sqlite3.Database(":memory:", (err) => {
+const dbPath = "C:\\Users\\chinc\\cat.db";
+let connection = null;
+const connectDB = () => {
+  connection = new sqlite3.Database(dbPath, (err) => {
     if (err) {
       return console.error(err.message);
     }
-    console.log("Connected to the in-memory SQlite database.");
+    console.log("Connected to the cat.db SQlite database.");
   });
-
-  // query data
-  db.serialize(function () {
-    db.each("SELECT name FROM cat", function (err, row) {
-      if (err) {
-        return console.error(err.message);
-      }
-      console.log(row.name);
-      //   result.push({ name: row.name });
-    });
-  });
-
-  // close the database connection
-  db.close((err) => {
+};
+const disconnectDB = () => {
+  connection.close((err) => {
     if (err) {
       return console.error(err.message);
     }
     console.log("Close the database connection.");
   });
-  return result;
+};
+
+const query = () => {
+  // query data
+  return new Promise((resolve, reject) => {
+    connection.serialize(function () {
+      connection.all("SELECT * FROM cats", function (err, rows) {
+        if (err) {
+          return reject(new Error(err.message));
+        }
+        resolve(rows);
+      });
+    });
+  });
 };
 
 const server = http.createServer((req, res) => {
@@ -68,13 +69,17 @@ const server = http.createServer((req, res) => {
         }
       );
       break;
-    case "/db":
+    case "/meow":
+      connectDB();
       res.writeHead(200, { "Content-Type": "application/json" });
-      res.end(
-        JSON.stringify({
-          data: query(),
+      query()
+        .then((result) => {
+          res.end(JSON.stringify({ data: result }));
         })
-      );
+        .catch((err) => {
+          res.end(JSON.stringify({ error: err.message }));
+        });
+      disconnectDB();
       break;
   }
 });
